d0c0e28f09716c4a6fb7b0796d8845a41211acdf
Maria Vintimilla <mariavintimilla@Marias-MacBook-Air.local>
2016-02-02 19:45:37 -0500

    edited webservercl.js

--- webservercl.js
+++ webservercl.js
@@ -3,20 +3,11 @@ var PORT = 7000;
 var PORT2 = 7500;
 
 
-var server = http.createServer(function(req, res){
-  res.write("sever is running")
-}
-
-  setTimeout(function){
-  res.end;
- },  7000);
-
- }) ;
-
-server.listen(PORT, function() {
-  console.log("you are doing wonderful today!", PORT);
-}
-
-server.listen(PORT2, function) {
-  console.log("you are so meh", PORT2);
+var goodServer = http.createServer(function(req, res){
+  res.end"Muy bien! Your sever is running!! :D", GOODPORT);
 });
+
+goodServer.listen(GOODPORT, function() {
+  console.log("server is listening at http://localhost:%s", GOODPORT);
+}})
+
