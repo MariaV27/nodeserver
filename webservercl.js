03c0c2fec5b2d5c66860f5174cc53dfd8070202a
Maria Vintimilla <mariavintimilla@Marias-MacBook-Air.local>
2016-02-02 19:49:14 -0500

    cleaned up webservercl.js

--- webservercl.js
+++ webservercl.js
@@ -4,10 +4,17 @@ var PORT2 = 7500;
 
 
 var goodServer = http.createServer(function(req, res){
-  res.end"Muy bien! Your sever is running!! :D", GOODPORT);
+  res.end("Muy bien! Your sever is running!! :D", GOODPORT);
 });
 
-goodServer.listen(GOODPORT, function() {
-  console.log("server is listening at http://localhost:%s", GOODPORT);
-}})
+goodServer.listen(GOODPORT, function(){
+    console.log("server is listening at http://localhost:%s", GOODPORT);
+})
 
+var badServer = http.createServer(function(req, res){
+    res.end("Your server's non performant bro " + GOODPORT);
+});
+
+badServer.listen(BADPORT, function(){
+    console.log("server is listening at http://localhost:%s", BADPORT);
+})
